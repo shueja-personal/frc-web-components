20552ac29ab4351460079295df1dad3ad3f575da
shueja-personal <j.a.shue@outlook.com>
2022-11-19 00:48:03 -0800

    fixes dashboard-tab dual-define

--- packages/frc-web-components/vite.config.js
+++ packages/frc-web-components/vite.config.js
@@ -8,4 +8,10 @@ module.exports = defineConfig({
       formats: ['es'],
     },
   },
+  server: {
+    fs: {
+      // Allow serving files from one level up to the project root
+      allow: ['.'],
+    },
+  },
 });
